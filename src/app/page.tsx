b93e693dd2e56ea63070636c51857844b0a06b2e
anavirginiacres <anavirginiacrespo@gmail.com>
2026-06-23 12:37:32 -0300

    initial hero banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import Link from "next/link";
+import Hero from "@/components/Hero/Hero";
 
 export default function HomePage() {
-  return (
-    <main>
-      <h1>Inicio</h1>
-      <p>Bienvenida/o a la demo de rutas con Next.js + TypeScript.</p>
-    </main>
-  );
+  return <Hero />;
 }
